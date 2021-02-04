b492bafb067632ec84ca8887275ee398976aa2f5
xiaofsu <xiaofsu@163.com>
2021-02-04 19:19:37 +0800

    change start.js

--- start.js
+++ start.js
@@ -1,16 +1,16 @@
 const schedule = require('node-schedule');
 
 var replyNum = 0, signNum = 0, browseNum = 0;
+var reply, browse, sign;
 
 // 每5分钟check一下需不需要回复
-schedule.scheduleJob('0 0/5 * * * *', () => {
+schedule.scheduleJob('0 0/1 * * * *', () => {
     console.log(`${new Date()} 开始执行 reply操作`);
     replyNum++;
     console.log(`第 ${replyNum} 次访问`);
     if (replyNum <= 1) {
-        require("./reply/reply.js");
+        reply = require("./reply/reply.js");
     } else {
-        var reply = require("./reply/reply.js");
         reply.reply();
     }
 });
@@ -20,9 +20,8 @@ schedule.scheduleJob('0 0 1 * * *', () => {
     console.log(`${new Date()} 开始执行 browse操作`);
     browseNum++;
     if (browseNum <= 1) {
-        require("./browse/browse.js");
+        browse = require("./browse/browse.js");
     } else {
-        var browse = require("./browse/browse.js");
         browse.browse();
     }
 });
@@ -32,9 +31,8 @@ schedule.scheduleJob('0 5 0 * * *', () => {
     console.log(`${new Date()} 开始执行 sign操作`);
     signNum++;
     if (signNum <= 1) {
-        require("./sign/sign.js");
+        sign = require("./sign/sign.js");
     } else {
-        var sign = require("./sign/sign.js");
         sign.sign();
     }
 });
